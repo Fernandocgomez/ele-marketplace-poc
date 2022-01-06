527ade6de4ebfc567119f580d0952b12745c1f32
fernandocgomez <fernandocgomez@live.com>
2022-01-06 02:05:25 -0600

    texas-plan-selection angular boiler plate app created

---
+++ apps/texas-plan-selection/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ele-marketplace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'texas-plan-selection';
+}
