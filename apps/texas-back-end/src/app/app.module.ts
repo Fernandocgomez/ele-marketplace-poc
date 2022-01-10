a30bf22ccd775f62c29e5dcb0ea5821e3a5a1fa9
fernandocgomez <fernandocgomez@live.com>
2022-01-10 16:39:02 -0600

    create two route for accessing and update the lead

--- apps/texas-back-end/src/app/app.module.ts
+++ apps/texas-back-end/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadController } from './controllers/lead/lead.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, LeadController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ apps/texas-back-end/src/app/controllers/lead/lead.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+
+interface Lead {
+    id: string;
+
+    [key: string]: any;
+}
+
+let data: Lead[] = [
+    {
+        "id": "1",
+        "email": "fernandocgomez@live.com"
+    },
+    {
+        "id": "2",
+        "email": "gomez@live.com"
+    },
+];
+
+
+@Controller('lead')
+export class LeadController {
+    @Get(':id')
+    async show(@Param() params) { 
+        const lead = await data.find((l) => {
+            return l.id === params.id;
+        })
+
+        return lead;
+    }
+
+    @Put()
+    async edit(@Body() newLead: Lead) {
+        const newData = await data.filter((l) => {
+            return l.id !== newLead.id;
+        });
+
+        data = newData;
+
+        data.push(newLead);
+
+        return newLead;
+
+    }
+}
