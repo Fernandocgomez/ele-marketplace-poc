5bdc13867e78c9a319a87cbfd2c2133753b550ec
fernandocgomez <fernandocgomez@live.com>
2022-01-09 20:18:23 -0600

    generate card component for texas-plan-selection

--- apps/texas-plan-selection/src/app/app.module.ts
+++ apps/texas-plan-selection/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { PlanCardComponent } from './components/plan-card/plan-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PlanCardComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
---
+++ apps/texas-plan-selection/src/app/components/plan-card/plan-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ele-marketplace-plan-card',
+  templateUrl: './plan-card.component.html',
+  styleUrls: ['./plan-card.component.scss']
+})
+export class PlanCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
